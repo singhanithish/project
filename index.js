8171b524df3fd5ceea57a43578baeb76cf5ced7e
Nithish Singha <singhanithish@gmail.com>
2021-06-26 03:09:23 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import APIApp from './Api'
+
+//import './index.css'
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
